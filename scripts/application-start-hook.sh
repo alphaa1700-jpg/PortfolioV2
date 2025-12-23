952bd6733b8da53f906a419755383db6790b9c5f
Alpha <alphaa1700@gmail.com>
2025-12-23 15:04:25 +0530

    Update application-start-hook.sh
    user changed from ubunutu to ec2-user

--- scripts/application-start-hook.sh
+++ scripts/application-start-hook.sh
@@ -2,13 +2,13 @@
 echo "started"
 pwd
 
-cd /home/ubuntu/Portfolio2 && npm install
+cd /home/ec2-user/Portfolio2 && npm install
 
-cd /home/ubuntu/Portfolio2 && npm install pm2
+cd /home/ec2-user/Portfolio2 && npm install pm2
 
 pwd
 
-cd /home/ubuntu/Portfolio2 && pm2 restart Portfolio2
+cd /home/ec2-user/Portfolio2 && pm2 restart Portfolio2
 
 pwd
 
