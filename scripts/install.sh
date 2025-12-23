3cf052efa474c94f5f662acb06fa05b0863b623c
alphaa1700-jpg <alphaa1700@gmail.com>
2025-12-23 12:56:21 +0530

    appspec updated

--- scripts/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-cd /home/ubuntu/Portfolio2
-npm install
-chmod +x scripts/install.sh
--- scripts/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-cd /home/ubuntu/Portfolio2
-pm2 delete portfolio || truepm2 start index.js --name portfolio
-pm2 save
-chmod +x scripts/start.sh
